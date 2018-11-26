c21fa2425058e460830ab0b811fa239c02e9bfab
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-26 15:09:53 -0800

    playing with routes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const auth = require('./controllers/auth')
 const app = express()
-
-
-
+const methodOverride = require('method-override')
 var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+const bodyParser = require('body-parser');
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 auth(app); 
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,14 @@
+
 module.exports = function (app) {
 
     // INDEX
     app.get('/', (req, res) => {
-        res.render('home');
+        res.render('home', {});
     })
+
+    // SHOW
+    app.get('/sign-up', (req, res) => {
+        res.render('form', {});
+    })
+
 }
